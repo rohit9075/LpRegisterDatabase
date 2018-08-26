0584551a1b6fe06aec21e4c5d8da63daded87fd0
Rohit Kumar <rohit9075@gmail.com>
2018-08-26 22:52:55 +0530

    code updates

--- app/src/main/java/com/rohit/lpregister/activity/RegisterActivity.java
+++ app/src/main/java/com/rohit/lpregister/activity/RegisterActivity.java
@@ -7,7 +7,6 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
